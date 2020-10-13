45ce012de612a46669a49c1569395e4b1fc6bcdd
jowpurcinelli <jppurcinelli@gmail.com>
2020-10-13 15:35:44 -0300

    InseringMap

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import './styles/global.css';
-import Landing from './pages/Landing'
 import Routes from './routes';
 
 
--- web/src/pages/OrphanagesMap/index.tsx
+++ web/src/pages/OrphanagesMap/index.tsx
@@ -1,11 +1,39 @@
 import React from 'react';
+import mapMarker from '../../images/map-marker.svg';
+import { Link } from 'react-router-dom';
+import {FiPlus} from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
+
+import'leaflet/dist/leaflet.css';
+import './styles.css';
 
 function OrphanagesMap( ) {
     return (
-        <div>
-            <h1>
-                Map
-            </h1>
+        <div id="map-page">
+            <aside>
+                <header>
+                    <img src={ mapMarker } alt="mapMarker"/>
+                    <h2> Elijas un orfanato en el mapa </h2>
+                    <p> Los chiquelines te están esperando! </p>
+                </header>
+                <footer>
+                    <strong> Montevideo </strong>
+                    <span> Uruguay </span>
+                </footer>
+            </aside>
+            <Map
+                center={[-34.896938,-56.1533621]}
+                zoom={13.5}
+                style={{width: '100%', height: '100%'}}
+            >
+                <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            </Map>
+
+            <Link to="" className="create-orphanage">
+                <FiPlus  size={32} color="#FFF" />
+            </Link>
+
+
         </div>
     )
 }
