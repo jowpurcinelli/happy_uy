7dcd4af7ae756a637c39caab4378a153978bdb30
jowpurcinelli <jppurcinelli@gmail.com>
2020-10-13 22:31:54 -0300

    ormDatabaseCreation

---
+++ backend/src/database/migrations/1602636230693-create_orphanages.ts
@@ -0,0 +1,61 @@
+import { text } from "express";
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class createOrphanages1602636230693 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        // realiza alteraciones que solicitamos en el banco de datos
+        await queryRunner.createTable( new Table( {
+            name: 'orphanages',
+            columns: [
+                {
+                    name: 'id',
+                    type: 'integer',
+                    unsigned: true, //osea, no puede ser negativa
+                    isPrimary: true,
+                    isGenerated: true,
+                    generationStrategy: 'increment', //para cada nuevo registro, sera generada utilizando una logica de imcrementacion, osea, aumenta el id en 1+
+
+                },
+                {
+                    name: 'name',
+                    type: 'varchar'
+                },
+                {
+                    name: 'latitude',
+                    type: 'decimal',
+                    scale: 10,
+                    precision: 2,
+                },
+                {
+                    name: 'longitude',
+                    type: 'decimal',
+                    scale: 10,
+                    precision: 2,
+                },
+                {
+                    name: 'about',
+                    type: 'text',
+                },
+                {
+                    name: 'instructions',
+                    type: 'text',
+                }, 
+                {
+                    name: 'open_on_weekends',
+                    type: 'boolean',
+                    default: false,
+                },                                     
+            ],
+        }))
+    
+    
+    
+    
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    // desace lo que fue hecho por el up
+    await queryRunner.dropTable( 'orphanages');
+    }
+
+}
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,12 @@
 import express from 'express';
 
+import './database/connection';
+
 const app = express ( );
+
+
+app.use(express.json( ));
+
 
 app.get('/users', ( request, response) => {
     return response.json({ message: 'Hello World' })
@@ -9,3 +15,17 @@ app.get('/users', ( request, response) => {
 
 
 app.listen(3333);
+
+
+/*
+Driver nativo:
+     permite ejecutar querys del banco de datos diectamente por el node, pero no tiene abstraccion
+-----------------
+Query builder (knex):
+    Permite escribir las querys utilizando syntax javascript
+----------------
+ORM (Object Relational Mapping):
+    Hace un relacionamento de objectos para cada  registro de una tabla (si tengo 3 users, orm vas a representalos como User User User, osea, 3 instancias/objectos de la clase user)
+
+
+    */
