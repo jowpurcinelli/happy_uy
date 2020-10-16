dfc0aecf56654825df09c5926a24ecf5352f5109
jowpurcinelli <jppurcinelli@gmail.com>
2020-10-15 23:20:31 -0300

    multerUpladPath

--- backend/src/config/upload.ts
+++ backend/src/config/upload.ts
@@ -1,2 +1,15 @@
 //package that deals with image upload
 
+import multer from 'multer';
+import path from 'path';
+
+export default {
+    storage: multer.diskStorage({
+        destination:  path.join(__dirname, ' ..', '..', 'uploads') ,// '..', '..' are for returnind pages to find the upload folder. Separated using commas because  the paths are differently deppending on the of the user. With the comma, path.join() can change itself :)   
+        filename: (request, file, cb) =>  {
+            const fileName = `${Date.now( )} - ${file.originalname}`;
+
+            cb(null, fileName)
+        },
+    })
+};
