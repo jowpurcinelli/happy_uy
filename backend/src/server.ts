cacf05f4d25005d51bdb6f4a81d5e089925c86f2
jowpurcinelli <jppurcinelli@gmail.com>
2020-10-16 01:31:04 -0300

    serverUploadconnection

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 
 import './database/connection';
 import routes from './routes';
@@ -9,6 +10,7 @@ const app = express ( );
 app.use(express.json( ));
 
 app.use(routes);
+app.use('/uplodas', express.static( path.join( __dirname, ' ..', 'uploads') ))
 
 app.listen(3333);
 
